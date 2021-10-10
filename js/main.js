bbc59d4119716a1000aaf6e7cf94a3032ff3c2fe
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-10-10 18:13:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,6 +11,10 @@ var swiper2 = new Swiper('.swiper2', {
   autoplay: {
     delay: 5000,
   },
+  pagination: {
+    el: '.swiper-pagination2',
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next2",
     prevEl: ".swiper-button-prev2",
@@ -20,8 +24,20 @@ var swiper3 = new Swiper('.swiper3', {
   autoplay: {
     delay: 5000,
   },
+  pagination: {
+    el: '.swiper-pagination3',
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next3",
     prevEl: ".swiper-button-prev3",
   },
+});
+
+
+$(document).ready(function () {
+  $('.menu__mob').click(function () {
+    $('.menu__list').toggleClass('open__menu');
+    $('.menu__mob').toggleClass('remove__menu');
+  });
 });
