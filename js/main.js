00b35a3ceb5fe8c75d74a957e99d5c294e5a0f75
Aneckon <83111616+Aneckon@users.noreply.github.com>
2021-10-14 13:54:41 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,40 +1,3 @@
-var swiper1 = new Swiper('.swiper1', {
-  pagination: {
-    el: '.swiper-pagination1',
-    clickable: true,
-  },
-  autoplay: {
-    delay: 5000,
-  },
-});
-var swiper2 = new Swiper('.swiper2', {
-  autoplay: {
-    delay: 5000,
-  },
-  pagination: {
-    el: '.swiper-pagination2',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next2",
-    prevEl: ".swiper-button-prev2",
-  },
-});
-var swiper3 = new Swiper('.swiper3', {
-  autoplay: {
-    delay: 5000,
-  },
-  pagination: {
-    el: '.swiper-pagination3',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next3",
-    prevEl: ".swiper-button-prev3",
-  },
-});
-
-
 $(document).ready(function () {
   $('.menu__mob').click(function () {
     $('.menu__list').toggleClass('open__menu');
@@ -71,6 +34,42 @@ $(document).ready(function () {
   });
 });
 
+
+var swiper1 = new Swiper('.swiper1', {
+  pagination: {
+    el: '.swiper-pagination1',
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+var swiper2 = new Swiper('.swiper2', {
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination2',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+});
+var swiper3 = new Swiper('.swiper3', {
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination3',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next3",
+    prevEl: ".swiper-button-prev3",
+  },
+});
 
 (function () {
 
